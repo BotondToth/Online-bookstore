7f64f300959aff2c2f685d205433a869e3998978
Roland <h672349@stud.u-szeged.hu>
2018-11-26 14:32:40 +0100

    Részletes keresés backend és frontend

--- src/main/java/hu/szte/bookstore/repository/BookRepository.java
+++ src/main/java/hu/szte/bookstore/repository/BookRepository.java
@@ -3,6 +3,7 @@ package hu.szte.bookstore.repository;
 import hu.szte.bookstore.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,6 +12,7 @@ import java.util.List;
  *
  * @author Botond
  */
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, String> {
 
@@ -18,13 +20,16 @@ public interface BookRepository extends JpaRepository<Book, String> {
 
     List<Book> findAllByAuthor(String author);
 
-    List<Book> findAllByTitleAndAuthor(String title, String author);
-
     List<Book> findAllByReleaseDate(int releaseDate);
 
     List<Book> findAllByReleaseDateBetween(int from, int to);
 
     Book findByIsbn(String isbn);
+
+
+    //@Query("SELECT b FROM Book b WHERE LOWER(b.title) = LOWER(:title) OR LOWER(b.author) = LOWER(:author) OR b.genre = (SELECT id FROM Genres where genre = :genre)")
+    @Query(value = "SELECT * FROM Books WHERE LOWER(title) = LOWER(:title) OR LOWER(author) = LOWER(:author) OR genre = (SELECT id FROM Genres where genre = :genre)", nativeQuery = true)
+    List<Book> findAllByTitleOrAuthorOrGenreIgnoreCase(@Param("title") String title, @Param("author") String author, @Param("genre") String genre);
 
     @Query("SELECT DISTINCT a.author FROM Book a")
     List<String> findAuthors();
